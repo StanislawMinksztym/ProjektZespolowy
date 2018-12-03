31452478cd484fa53bb61aa233a2ce83a9f2d5d0
Stanisław Minksztym <stanislaw.minksztym@gmail.com>
2018-12-03 22:07:50 +0100

    kek, to chyba działa

---
+++ Ankiety-PZ/Ankiety-PZ/Controllers/AnkietyController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ankiety_PZ.Models;
+
+namespace Ankiety_PZ.Controllers
+{
+    public class AnkietyController : Controller
+    {
+
+        private Entities databaseManager = new Entities();
+
+        // GET: Ankiety
+        public ActionResult Index()
+        {
+
+            //TEST:
+            var a = "";
+            using (Entities ctx = new Entities())
+            {
+                var result = from t in ctx.WyswietlAnkiety
+                             select t;
+                foreach (WyswietlAnkiety t in result)
+                {
+                    a = a + t.NazwaAnkiety + ",";
+                }
+            }
+
+            ViewBag.TEST = a;
+
+            return View();
+        }
+
+    }
+}
