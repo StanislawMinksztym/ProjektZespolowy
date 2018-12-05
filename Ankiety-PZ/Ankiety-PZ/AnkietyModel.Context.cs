fe7697d0fdd59cc06396dd88445fa6c800e31a73
Stanisław Minksztym <stanislaw.minksztym@gmail.com>
2018-12-05 15:26:37 +0100

    123

--- Ankiety-PZ/Ankiety-PZ/AnkietyModel.Context.cs
+++ Ankiety-PZ/Ankiety-PZ/AnkietyModel.Context.cs
@@ -42,5 +42,110 @@ namespace Ankiety_PZ
     
             return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<WypelnijAnkiete_Result>("WypelnijAnkiete", idankietyParameter);
         }
+    
+        public virtual int AktualizujWynik(Nullable<int> idankiety, Nullable<int> idpytania, Nullable<int> nrodpowiedzi)
+        {
+            var idankietyParameter = idankiety.HasValue ?
+                new ObjectParameter("idankiety", idankiety) :
+                new ObjectParameter("idankiety", typeof(int));
+    
+            var idpytaniaParameter = idpytania.HasValue ?
+                new ObjectParameter("idpytania", idpytania) :
+                new ObjectParameter("idpytania", typeof(int));
+    
+            var nrodpowiedziParameter = nrodpowiedzi.HasValue ?
+                new ObjectParameter("nrodpowiedzi", nrodpowiedzi) :
+                new ObjectParameter("nrodpowiedzi", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("AktualizujWynik", idankietyParameter, idpytaniaParameter, nrodpowiedziParameter);
+        }
+    
+        public virtual int DodajAnkiete(string nazwa, Nullable<int> iduz)
+        {
+            var nazwaParameter = nazwa != null ?
+                new ObjectParameter("nazwa", nazwa) :
+                new ObjectParameter("nazwa", typeof(string));
+    
+            var iduzParameter = iduz.HasValue ?
+                new ObjectParameter("iduz", iduz) :
+                new ObjectParameter("iduz", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("DodajAnkiete", nazwaParameter, iduzParameter);
+        }
+    
+        public virtual int DodajPytanie(Nullable<int> idankiety, Nullable<bool> kilka, string pytanie, string odp1, string odp2, string odp3, string odp4, string odp5, string odp6, string odp7, string odp8, string odp9, string odp10)
+        {
+            var idankietyParameter = idankiety.HasValue ?
+                new ObjectParameter("idankiety", idankiety) :
+                new ObjectParameter("idankiety", typeof(int));
+    
+            var kilkaParameter = kilka.HasValue ?
+                new ObjectParameter("kilka", kilka) :
+                new ObjectParameter("kilka", typeof(bool));
+    
+            var pytanieParameter = pytanie != null ?
+                new ObjectParameter("pytanie", pytanie) :
+                new ObjectParameter("pytanie", typeof(string));
+    
+            var odp1Parameter = odp1 != null ?
+                new ObjectParameter("odp1", odp1) :
+                new ObjectParameter("odp1", typeof(string));
+    
+            var odp2Parameter = odp2 != null ?
+                new ObjectParameter("odp2", odp2) :
+                new ObjectParameter("odp2", typeof(string));
+    
+            var odp3Parameter = odp3 != null ?
+                new ObjectParameter("odp3", odp3) :
+                new ObjectParameter("odp3", typeof(string));
+    
+            var odp4Parameter = odp4 != null ?
+                new ObjectParameter("odp4", odp4) :
+                new ObjectParameter("odp4", typeof(string));
+    
+            var odp5Parameter = odp5 != null ?
+                new ObjectParameter("odp5", odp5) :
+                new ObjectParameter("odp5", typeof(string));
+    
+            var odp6Parameter = odp6 != null ?
+                new ObjectParameter("odp6", odp6) :
+                new ObjectParameter("odp6", typeof(string));
+    
+            var odp7Parameter = odp7 != null ?
+                new ObjectParameter("odp7", odp7) :
+                new ObjectParameter("odp7", typeof(string));
+    
+            var odp8Parameter = odp8 != null ?
+                new ObjectParameter("odp8", odp8) :
+                new ObjectParameter("odp8", typeof(string));
+    
+            var odp9Parameter = odp9 != null ?
+                new ObjectParameter("odp9", odp9) :
+                new ObjectParameter("odp9", typeof(string));
+    
+            var odp10Parameter = odp10 != null ?
+                new ObjectParameter("odp10", odp10) :
+                new ObjectParameter("odp10", typeof(string));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("DodajPytanie", idankietyParameter, kilkaParameter, pytanieParameter, odp1Parameter, odp2Parameter, odp3Parameter, odp4Parameter, odp5Parameter, odp6Parameter, odp7Parameter, odp8Parameter, odp9Parameter, odp10Parameter);
+        }
+    
+        public virtual ObjectResult<WyswietlPytania_Result> WyswietlPytania(Nullable<int> idankiety)
+        {
+            var idankietyParameter = idankiety.HasValue ?
+                new ObjectParameter("idankiety", idankiety) :
+                new ObjectParameter("idankiety", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<WyswietlPytania_Result>("WyswietlPytania", idankietyParameter);
+        }
+    
+        public virtual ObjectResult<WyswietlWynik_Result> WyswietlWynik(Nullable<int> idankiety)
+        {
+            var idankietyParameter = idankiety.HasValue ?
+                new ObjectParameter("idankiety", idankiety) :
+                new ObjectParameter("idankiety", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<WyswietlWynik_Result>("WyswietlWynik", idankietyParameter);
+        }
     }
 }
--- Ankiety-PZ/Ankiety-PZ/Controllers/AnkietyController.cs
+++ Ankiety-PZ/Ankiety-PZ/Controllers/AnkietyController.cs
@@ -12,41 +12,6 @@ namespace Ankiety_PZ.Controllers
         private Entities databaseManager = new Entities();
 
         // GET: Ankiety
-        public ActionResult Index()
-        {
-
-            //TEST1:
-            var a = "";
-            using (Entities ctx1 = new Entities())
-            {
-                var result = from t in ctx1.WyswietlAnkiety
-                             select t;
-                foreach (WyswietlAnkiety t in result)
-                {
-                    a = a + t.NazwaAnkiety + ",";
-                }
-            }
-
-            //TEST2:
-            var b = "";
-            Entities ctx2 = new Entities();
-
-            var pytaniaList = ctx2.WypelnijAnkiete(1).ToList();
-
-
-            foreach (WypelnijAnkiete_Result p in pytaniaList)
-            {
-
-                b = b + p.NazwaAnkiety + "," + p.Pytanie +"," + p.Odp_1 + "," + p.Odp_2 + ","+p.Odp_3 + "," +p.Odp_4+ "," +p.Odp_5+ ","+p.Odp_6 + "," + p.Odp_7 + "," + p.Odp_8 + "," + p.Odp_9 + "," + p.Odp_10;
-            }
-
-            ViewBag.TEST1 = a;
-
-            ViewBag.TEST2 = b;
-
-            return View();
-        }
-
         public ActionResult ListaAnkiet()
         {
             var ankietyList = new List<WyswietlAnkiety>();
@@ -59,35 +24,28 @@ namespace Ankiety_PZ.Controllers
                 {
                     ankietyList.Add(t);
                 }
-                
+
             }
 
             ViewBag.ListaAnkiet = ankietyList;
 
             return View();
         }
-
-        public ActionResult ListaAnkietLogout()
+        
+        public ActionResult Wyniki(int id)
         {
-
-            //TEST1:
-            var a = "";
-
-            var ankietyList = new List<WyswietlAnkiety>();
-
-            using (Entities ctx1 = new Entities())
+            using (var ctx = new Entities())
             {
-                var result = from t in ctx1.WyswietlAnkiety
-                             select t;
-                foreach (WyswietlAnkiety t in result)
-                {
-                    ankietyList.Add(t);
-                }
+                var wyniki = ctx.WyswietlWynik(id).ToList();
 
+                ViewBag.Wyniki = wyniki;
             }
 
-            ViewBag.ListaAnkiet = ankietyList;
+            return View();
+        }
 
+        public ActionResult WypelnijAnkiete()
+        {
             return View();
         }
 
