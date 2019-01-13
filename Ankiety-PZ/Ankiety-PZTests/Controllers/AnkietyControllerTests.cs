1fada17239558c6d85fdb5e8c362b1ee0429e05e
Tomek <tzajdzinski@gmail.com>
2019-01-13 08:58:01 +0100

    dodanie testów jednostkowych

--- Ankiety-PZ/Ankiety-PZTests/Controllers/AnkietyControllerTests.cs
+++ Ankiety-PZ/Ankiety-PZTests/Controllers/AnkietyControllerTests.cs
@@ -2,10 +2,13 @@
 using Ankiety_PZ.Controllers;
 using System;
 using System.Collections.Generic;
+using Moq;
+using System.Web;
 using System.Linq;
 using System.Text;
 using System.Web.Mvc;
 using System.Threading.Tasks;
+using System.Collections.Specialized;
 
 namespace Ankiety_PZ.Controllers.Tests
 {
@@ -25,37 +28,70 @@ namespace Ankiety_PZ.Controllers.Tests
         [TestMethod()]
         public void WynikiTest()
         {
-            Assert.Fail();
-        }
+            var controller = new AnkietyController();
+            var context = MockRepository.GenerateStub<ControllerContext>();
+            context.Expect(x => x.HttpContext.Session["MyKey"]).Return("MyValue");
+            controller.ControllerContext = context;
+            controller.Wyniki(1);
 
-        [TestMethod()]
-        public void WypelnijAnkieteTest()
-        {
-            Assert.Fail();
-        }
-
-        [TestMethod()]
-        public void AktualizujWynikTest()
-        {
-            Assert.Fail();
-        }
-
-        [TestMethod()]
-        public void DodajAnkieteTest()
-        {
-            Assert.Fail();
+            Assert.IsNotNull(controller);
         }
 
         [TestMethod()]
         public void ZapiszAnkieteTest()
         {
-            Assert.Fail();
+            var request = new Mock<HttpRequestBase>();
+            var context = new Mock<HttpContextBase>();
+            var controller = new AnkietyController();
+            var session = new Mock<HttpSessionStateBase>();
+
+            var sessionDict = new Dictionary<string, string>
+            {
+                {"UserId", "1" },
+            };
+
+            controller.ControllerContext = new ControllerContext
+            {
+                Controller = controller,
+                HttpContext = context.Object,
+            };
+            var ankieta = new NameValueCollection();
+            ankieta.Add("NazwaAnkiety", "Ankieta testowa");
+            
+            controller.Session.Add("UserId", "1");
+            context.Setup(c => c.Request.Form).Returns(ankieta);
+            var result = controller.ZapiszAnkiete();
+
+            Assert.AreNotEqual(0, result);
         }
 
         [TestMethod()]
         public void ZapiszPytanieTest()
         {
-            Assert.Fail();
+            var request = new Mock<HttpRequestBase>();
+            var context = new Mock<HttpContextBase>();
+            var controller = new AnkietyController();
+
+            var pytanie = new NameValueCollection();
+            pytanie.Add("odp_1", "odpowiedź 1");
+            pytanie.Add("odp_2", "odpowiedź 2");
+            pytanie.Add("odp_3", "odpowiedź 3");
+            pytanie.Add("odp_4", "odpowiedź 4");
+            pytanie.Add("odp_5", "odpowiedź 5");
+            pytanie.Add("odp_6", "odpowiedź 6");
+            pytanie.Add("wielo", "true");
+            pytanie.Add("id_ankiety", "2");
+            pytanie.Add("pyt1", "pytanie_testowe");
+
+            context.Setup(m => m.Request.Form).Returns(pytanie);
+            controller.ControllerContext = new ControllerContext
+            {
+                Controller = controller,
+                HttpContext = context.Object
+            };
+            var result = controller.ZapiszPytanie();
+
+            Assert.AreNotEqual(0, result);
         }
     }
 }
